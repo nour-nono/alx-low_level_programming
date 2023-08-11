215d43fb60dc9b51153dcf0b0938f3bb1970539b
nono <nour98nour@gmail.com>
2023-08-11 14:58:27 -0700

    ended task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		++n;
+	}
+	n = 'A';
+	while (n <= 'Z')
+        {
+                putchar(n);
+                ++n;
+        }
+	putchar('\n');
+	return (0);
+}
