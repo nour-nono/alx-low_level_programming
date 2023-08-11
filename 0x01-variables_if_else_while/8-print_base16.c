e5ecf54a9e4ec250c2cd932a1e72c905ad815d6b
nono <nour98nour@gmail.com>
2023-08-11 15:46:12 -0700

    solved mand projects pt1

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar('0' + n);
+		++n;
+	}
+	n = 0;
+	while (n <= 5)
+	{
+		putchar('a' + n);
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,27 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
