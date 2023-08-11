f39d404a33b646a2ad58df4e7a267bf34c3c0044
nono <nour98nour@gmail.com>
2023-08-11 14:36:54 -0700

    solved the 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,33 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program find if the number is greater than 5 or not if its
+ * 0 or not
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is ", n);
+	if (n < 0)
+	{
+		printf("-%d and is less than 6 and not 0\n", n % 10);
+	} else
+	{
+		if (n % 10 == 0)
+			printf("%d and is 0\n", n % 10);
+		else if (n % 10 > 5)
+			printf("%d and is greater than 5\n", n % 10);
+		else if (n % 10 < 6)
+			printf("%d and is less than 6 and not 0\n", n % 10);
+	}
+	return (0);
+}
