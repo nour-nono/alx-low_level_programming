c1cab632b1a03a5fef3805cc9980218c429a5497
nono <nour98nour@gmail.com>
2023-08-11 17:03:37 -0700

    solved all adv but not the last one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,31 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = 0, m;
+
+	while (n <= 9)
+	{
+		m = n + 1;
+		while (m <= 9)
+		{
+		putchar('0' + n);
+		putchar('0' + m);
+		putchar(',');
+		putchar(' ');
+		++m;
+		}
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,37 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = 0, m, o;
+
+	while (n <= 9)
+	{
+		m = n + 1;
+		while (m <= 9)
+		{
+			o = m + 1;
+			while (o <= 9)
+			{
+				putchar('0' + n);
+				putchar('0' + m);
+				putchar('0' + o);
+				putchar(',');
+				putchar(' ');
+				++o;
+			}
+			++m;
+		}
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
