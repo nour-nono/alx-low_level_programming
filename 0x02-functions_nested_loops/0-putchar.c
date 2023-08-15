2230f8098ac67aa92463c4a9b188222a3fec4479
nono <nour98nour@gmail.com>
2023-08-15 07:05:04 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
