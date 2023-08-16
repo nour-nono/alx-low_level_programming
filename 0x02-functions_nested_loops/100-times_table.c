23bdade582dabe19d352b5ae476eecc7af7de806
nono <nour98nour@gmail.com>
2023-08-16 09:03:40 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,11 +25,11 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				if (z >= 100)
-                                        _putchar('0' + (z / 100));
-                                else
-                                        _putchar(' ');
+					_putchar('0' + (z / 100));
+				else
+					_putchar(' ');
 				if (z >= 10)
-					_putchar('0' + ((z % 100)/ 10));
+					_putchar('0' + ((z % 100) / 10));
 				else
 					_putchar(' ');
 				_putchar('0' + (z % 10));
