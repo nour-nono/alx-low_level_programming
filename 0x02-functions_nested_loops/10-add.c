266348d05fb38c116974da357420f29bedbeecd0
nono <nour98nour@gmail.com>
2023-08-15 17:39:26 -0700

    A

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- * print_last_digit - Entry point
+ * add - Entry point
  *
  * @n: is a num
+ *
+ * @m: is a num
  *
  * Description: this program prints "_putchar"
  *
  * Return: Always 0 (Success)
 */
-int print_last_digit(int n)
+int add(int n, int m)
 {
-	int z;
-
-	z = n % 10;
-	if (z < 0)
-		z = -1 * z;
-	_putchar('0' + z);
-	return (z);
+	return (n + m);
 }
 
