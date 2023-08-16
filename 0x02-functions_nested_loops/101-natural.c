6822815ae2bc44b1768cf0cc8476dfa261781b6e
nono <nour98nour@gmail.com>
2023-08-16 11:17:50 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int sum = 0, i = 5, j = 3;
+
+	for (; i < 1024; i += 5)
+		sum += i;
+	for (; j < 1024; j += 3)
+		sum += j;
+	printf("%d\n", sum);
+	return (0);
+}
