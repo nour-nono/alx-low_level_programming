ba21460a089cb6a6d8fdd04519f8f5bc15c525cb
nono <nour98nour@gmail.com>
2023-08-17 13:10:24 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @n: is a num
+ *
+ * @m: is a num
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
