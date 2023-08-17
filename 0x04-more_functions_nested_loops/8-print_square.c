56f6b0266a1b7ba3c66c04053da843356b16aea0
nono <nour98nour@gmail.com>
2023-08-17 14:50:13 -0700

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *
+ * @size: is a num
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+void print_square(int size)
+{
+	int i = 0, j = 0;
+
+	if (size > 0)
+	{
+		for (; i < size; ++i)
+			for (; j < size; ++j)
+				_putchar('#');
+		_putchar('\n');
+		j = 0;
+	}
+	else
+		_putchar('\n');
+}
