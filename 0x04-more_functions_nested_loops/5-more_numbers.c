52476b140938e52935354b1e217cd8d6297f7cf4
nono <nour98nour@gmail.com>
2023-08-17 13:48:43 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,11 +18,8 @@ void more_numbers(void)
 		for (; i <= 14; ++i)
 		{
 			if (i > 9)
-			{
 				_putchar('0' + i / 10);
-			}
-			else
-				_putchar('0' + i % 10);
+			_putchar('0' + i % 10);
 		}
 		_putchar('\n');
 	}
