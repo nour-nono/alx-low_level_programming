7241f82c923be1975c9fb0a0c5dd95257f40e610
nono <nour98nour@gmail.com>
2023-08-20 04:59:42 -0700

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
 /**
+ * isprime - for prime nums
+ *
+ * @n: num
+ *
+ * Return: true for prime nums and false for other
+*/
+
+bool isprime(int n)
+{
+	if (n == 2)
+		return (true);
+	else if (n == 1 || n % 2 == 0)
+		return (false);
+	for (int i = 3; i * i <= n; i += 2)
+		if (n % i == 0)
+			return (false);
+	return (true);
+}
+
+/**
  * main - Entry point
  *
  * Description: this program prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
 */
-bool isprime(int n)
-{
-	if (n == 2)
-		return true;
-	else if (n == 1 || n % 2 == 0)
-		return false;
-	for (int i = 3; i * i <= n; i += 2)
-		if (n % i == 0)
-			return false;
-	return true;
-}
-
 int main(void)
 {
 	int i = 782847;
@@ -27,8 +35,8 @@ int main(void)
 	{
 		if (isprime(i))
 		{
-				printf("%d", i);
-				break;
+			printf("%d", i);
+			break;
 		}
 	}
 	return (0);
