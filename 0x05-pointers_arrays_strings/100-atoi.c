ce939366cea269514c40af5e5d8d2099a06d7ed5
nono <nour98nour@gmail.com>
2023-08-21 16:02:32 -0700

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	int num = 0;
+	int cnt = 0;
+
+	while (s[i] > '9' || s[i] < '0')
+	{
+		if (s[i] == '-')
+		++cnt;
+		++i;
+	}
+	while (s[i] != '\0' && s[i] <= '9' && s[i] >= '0')
+	{
+		num += s[i] - '0';
+		num *= 10;
+		++i;
+	}
+	num /= 10;
+	if (cnt % 2)
+		num *= -1;
+	return (num);
+}
+
