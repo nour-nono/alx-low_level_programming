3d5077efd6548e557cbcb5a33db9e0e316cb3265
nono <nour98nour@gmail.com>
2023-08-21 16:16:09 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	int num = 0;
 	int cnt = 0;
 
-	while (s[i] > '9' || s[i] < '0')
+	while (s[i] != '\0' && (s[i] > '9' || s[i] < '0'))
 	{
 		if (s[i] == '-')
 		++cnt;
@@ -24,6 +24,7 @@ int _atoi(char *s)
 	while (s[i] != '\0' && s[i] <= '9' && s[i] >= '0')
 	{
 		num += s[i] - '0';
+		if (num < (INT_MAX / 10))
 		num *= 10;
 		++i;
 	}
