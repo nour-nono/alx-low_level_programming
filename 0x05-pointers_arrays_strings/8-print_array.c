7a065074c46025d407cd0bf4a91f6596f160932e
nono <nour98nour@gmail.com>
2023-08-21 11:55:25 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ *
+ * @a: is a pointer to arr of int
+ *
+ * @n: nums
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n - 1; i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[i])
+}
