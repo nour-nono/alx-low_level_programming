f58878cf517786c1e961722d581ab1813300f72c
nono <nour98nour@gmail.com>
2023-08-21 08:18:35 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _strlen - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+	return (sizeof(s) / sizeof(char));
+}
+
