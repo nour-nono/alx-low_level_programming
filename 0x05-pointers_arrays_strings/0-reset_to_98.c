737d25de7c4f70fd16ec95c70817e19f0cb979fc
nono <nour98nour@gmail.com>
2023-08-21 06:28:50 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
