4cae38e224769ccf2fc79fc77470ebb7455671b3
nono <nour98nour@gmail.com>
2023-08-21 06:34:25 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: is a pointer
+ *
+ * @b: another pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	
+	*a = *b;
+	*b = tmp;
+}
+
