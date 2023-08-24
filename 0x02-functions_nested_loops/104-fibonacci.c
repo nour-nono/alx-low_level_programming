d4e9d4e4e4345a4f20c811f1679a6ceaf8997f7a
nono <nour98nour@gmail.com>
2023-08-24 07:27:29 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,42 @@
 */
 int main(void)
 {
-	unsigned long int tmp = 0, i = 1, j = 2, n = 0;
+	long int a1 = 1, b1 = 2, a2 = 0, b2 = 0;
+	long int a3 = 0, b3 = 0, j = 0;
 
-	printf("%lu, %lu, ", i, j);
-	for (; n < 98; ++n)
+	for (; j < 97; ++j)
 	{
-		tmp = j;
-		j += i;
-		i = tmp;
-		printf("%lu, ", j);
+		int tmp1 = b1;
+		int tmp2 = b2;
+		int tmp3 = b3;
+
+		a1 = tmp1;
+		b1 += a1;
+		b2 += a2;
+		a2 = tmp2;
+		b3 += a3;
+		a3 = tmp3;
+		if (b1 >= 1e8)
+		{
+			b2 += b1 / 10000000;
+			b1 %= 10000000;
+		}
+		if (b2 >= 1e8)
+		{
+			b3 += b2 / 10000000;
+			b2 %= 10000000;
+		}
+		if (b3)
+		{
+			if (j < 96)
+				printf("%ld%ld%ld, ", b3, b2, b1);
+			else
+				printf("%ld%ld%ld", b3, b2, b1);
+		}
+		else if (b2)
+			printf("%ld%ld, ", b2, b1);
+		else
+			printf("%d, ", b1);
 	}
-	j += i;
-	printf("%lu\n", j);
 	return (0);
 }
