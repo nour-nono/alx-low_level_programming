05174653fa59c49e47eb55dd25b0e8bb69bef354
nono <nour98nour@gmail.com>
2023-08-27 08:57:34 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+*/
+
+int main(void)
+{
+	int sum = 0;
+	char ch;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		ch = random() % 128;
+		sum += ch;
+		putchar(ch);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
