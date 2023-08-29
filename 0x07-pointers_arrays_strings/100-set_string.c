cb1d7a288b1a0ca764f43805d89f7a648d0bb419
nono <nour98nour@gmail.com>
2023-08-29 09:12:49 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - print characters
+ *
+ * @s: arr
+ * @to: arr size
+ *
+ * Return: the pos of the character.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = &to[0];
+}
