e9c530fe66cff7f11cc0058e46a530e194347320
nono <nour98nour@gmail.com>
2023-08-28 18:39:46 -0700

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_diagsums - print characters
  *
@@ -7,10 +8,11 @@
  * @size: arr size
  *
  * Return: the pos of the character.
- */
+*/
+
 void print_diagsums(int *a, int size)
 {
-	int i = 0, i2 = size - 1, j = 0, sum1 = 0, sum2 = 0;
+	int i = 0, i2 = size - 1, sum1 = 0, sum2 = 0;
 
 	while (i != size)
 	{
