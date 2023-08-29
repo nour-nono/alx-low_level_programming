4d80cf4c72136931c1f36ee9fda8018aaddeb457
nono <nour98nour@gmail.com>
2023-08-28 17:36:44 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Find first occurrence of a specific character
+ *
+ * @haystack: string
+ * @needle: the string that we looking for
+ *
+ * Return: the pos of the character.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0, ans = 0;
+
+	while (haystack[i] != '\0')
+	{
+		int z = i, j = 0;
+
+		if (needle[j] == haystack[z])
+			while (needle[j] != '\0' && haystack[j] != '\0' && needle[j] == haystack[z])
+				++z, ++j;
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+		++i;
+	}
+	return ('\0');
+}
