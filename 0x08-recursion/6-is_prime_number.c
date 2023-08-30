56eb15c38f8d416db184c39926fff770f679fe09
nono <nour98nour@gmail.com>
2023-08-30 16:54:18 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,31 @@
 #include "main.h"
 /**
+ * ppr - check the code
+ * @n: num
+ * @i: for loop
+ * Return: true and false
+ */
+int ppr(int n, int i)
+{
+	if (i * i <= n)
+		if (n % i == 0)
+			return (0);
+		else
+			return (ppr(n, i + 2));
+	return (1);
+}
+/**
  * is_prime_number - check the code
  * @n: num
  * Return: true and false
  */
 int is_prime_number(int n)
 {
-	int i = 3;
-
 	if (n == 2)
 		return (1);
 	else if (n == 3)
 		return (1);
 	else if (n < 2 || n % 2 == 0)
 		return (0);
-	for (; i * i < n; i += 2)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	return (ppr(n, 3));
 }
