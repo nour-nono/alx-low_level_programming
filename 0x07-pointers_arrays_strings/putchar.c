713503502f12a8c3a1ade7c16b2cef6906dbdc41
nono <nour98nour@gmail.com>
2023-08-30 09:00:17 -0700

    a

---
+++ 0x07-pointers_arrays_strings/putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Prints character
+ *
+ * @c: the character to printed
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: 1 on (Success)
+ * and return -1 on error
+*/
+char _putchar(char c)
+{
+	return (putchar(c));
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+    putchar(*s);
+    s++;
+    if (*s != '\0')
+        _puts_recursion(s);
+    else
+        putchar('\n');
+}
