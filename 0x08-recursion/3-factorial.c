4b1d74922d67276769806954375b12dadce6cf31
nono <nour98nour@gmail.com>
2023-08-30 11:49:30 -0700

    a

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,13 @@
  */
 int factorial(int n)
 {
+	int sum = 1;
+
 	if (n > 0)
-		return (n * factorial(n - 1));
+		sum = n * factorial(n - 1);
 	else if (n == 0)
-		return (1);
+		sum = 1;
 	else if (n < 0)
-		return (-1);
+		sum = -1;
+	return (sum);
 }
