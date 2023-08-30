a15275fdc5c0ddfd3b53ce0b39565786b7543bfb
nono <nour98nour@gmail.com>
2023-08-30 16:30:38 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,14 +6,14 @@
  */
 int is_prime_number(int n)
 {
+	int i = 3;
+
 	if (n == 2)
 		return (1);
 	else if (n == 3)
 		return (1);
 	else if (n < 2 || n % 2 == 0)
 		return (0);
-	int i = 3;
-
 	for (; i * i < n; i += 2)
 	{
 		if (n % i == 0)
