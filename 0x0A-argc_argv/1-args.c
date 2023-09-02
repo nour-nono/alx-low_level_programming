d07d953bc08f3386affa19f15aae6ce8302c718c
nono <nour98nour@gmail.com>
2023-09-01 18:22:30 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void) argv;
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int a, b;
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	else
+	{
+		puts("Error");
+		return (1);
+	}
+}
+
