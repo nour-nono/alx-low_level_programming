b1a064279dea3de0e42d65f0f361865eb30a03fb
nono <nour98nour@gmail.com>
2023-09-05 07:31:47 -0700

    A

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,24 @@
 #include "main.h"
+
 /**
- * _strdup -  creates an array of chars, and initializes it with a specific char
+ * _strdup - copy string in heap
  * @str: string
  *
  * Return: string.
  */
+
 char *_strdup(char *str)
 {
 	unsigned int i = 0, j = 0;
 	char *ch;
+
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 	while (str[i] != '\0')
 		++i;
 	ch = (char *) malloc(i * sizeof(char));
 	if (ch == NULL)
-		return NULL;
+		return (NULL);
 	while (j <= i)
 	{
 		ch[j] = str[j];
