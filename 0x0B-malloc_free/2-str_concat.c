e3ddd0f9def565cc980e4118652324320cff4ed6
nono <nour98nour@gmail.com>
2023-09-05 15:53:06 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,17 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0;
 	char *ch;
 
-	while (s1 != NULL && s1[i] != '\0')
+	if (s1 == NULL)
+		*s1 = '\0';
+	if (s2 == NULL)
+		*s2 = '\0';
+	while (s1[i] != '\0')
 		++i;
-	while (s2 != NULL && s2[j] != '\0')
+	while (s2[j] != '\0')
 		++j;
-	if (s1 != NULL && s1[0] != '\0')
+	if (s1[0] != '\0')
 		++i;
-	if (s2 != NULL && s2[0] != '\0')
+	if (s2[0] != '\0')
 		++j;
 	if (i + j == 0)
 	{
@@ -30,12 +34,12 @@ char *str_concat(char *s1, char *s2)
 	i = 0, j = 0;
 	if (ch == NULL)
 		return (NULL);
-	while (s1 != NULL && s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		ch[i] = s1[i];
 		++i;
 	}
-	while (s2 != NULL && s2[j] != '\0')
+	while (s2[j] != '\0')
 	{
 		ch[i] = s2[j];
 		++i, ++j;
