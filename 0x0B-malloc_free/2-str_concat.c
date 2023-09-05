d2643d93730d7919dfd9bab3af4449da6301e17d
nono <nour98nour@gmail.com>
2023-09-05 16:03:50 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,16 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *ch;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = '\0';
 	if (s2 == NULL)
-		*s2 = '\0';
-	while (s1[i] != '\0')
+		s2 = '\0';
+	while (s1 == NULL && s1[i] != '\0')
 		++i;
-	while (s2[j] != '\0')
-		++j;
-	if (s1[0] != '\0')
-		++i;
-	if (s2[0] != '\0')
+	while (s2 == NULL && s2[j] != '\0')
 		++j;
 	if (i + j == 0)
 	{
@@ -30,16 +26,16 @@ char *str_concat(char *s1, char *s2)
 		*ch = '\0';
 	}
 	else
-		ch = (char *) malloc((i + j + 2) * sizeof(char));
+		ch = (char *) malloc((i + j + 1) * sizeof(char));
 	i = 0, j = 0;
 	if (ch == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (s1 == NULL && s1[i] != '\0')
 	{
 		ch[i] = s1[i];
 		++i;
 	}
-	while (s2[j] != '\0')
+	while (s2 == NULL && s2[j] != '\0')
 	{
 		ch[i] = s2[j];
 		++i, ++j;
