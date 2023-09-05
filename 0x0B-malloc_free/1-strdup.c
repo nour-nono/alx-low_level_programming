04fec3445909e5e1aea89f10fbd1ee6afe86ccf6
nono <nour98nour@gmail.com>
2023-09-05 07:20:06 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strdup -  creates an array of chars, and initializes it with a specific char
+ * @str: string
+ *
+ * Return: string.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *ch;
+
+	while (str[i] != '\0')
+		++i;
+	if (i == 0)
+		return NULL;
+
+	ch = (char *) malloc(i * sizeof(char));
+	if (ch == NULL)
+		return NULL;
+	while (j <= i)
+	{
+		ch[j] = str[j];
+		++j;
+	}
+	return (ch);
+}
