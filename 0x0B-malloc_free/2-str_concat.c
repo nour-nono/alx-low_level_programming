15ea2f91e2e9257ec60d8cede97a4ebd4cbbbe9a
nono <nour98nour@gmail.com>
2023-09-05 15:40:42 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 		++i;
 	while (s2 != NULL && s2[j] != '\0')
 		++j;
-	if (s1 != NULL)
+	if (s1 != NULL && s1[0] != '\0')
 		++i;
-	if (s2 != NULL)
+	if (s2 != NULL && s2[0] != '\0')
 		++j;
 	if (i + j == 0)
 	{
