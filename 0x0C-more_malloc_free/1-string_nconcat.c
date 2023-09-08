263d0e9f9a9be838fff98f8f618554af7b8a067e
nono <nour98nour@gmail.com>
2023-09-07 17:09:51 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,8 +39,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; ++i)
 			str[i] = s1[i];
-	for (j = 0; j < n; ++j, ++i)
+	for (j = 0; j < n; ++j)
+	{
 		str[i] = s2[j];
+		++i;
+	}
 	str[i] = '\0';
 	return (str);
 }
