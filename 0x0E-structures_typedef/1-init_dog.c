9ece4e85b220931beef57d5cb72bac21607ff3c5
nono <nour98nour@gmail.com>
2023-09-11 16:36:09 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - just a function
+* @d: struct
+* @name: the dog name
+* @age: the dog age
+* @owner: the dog owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = *name;
+	d->owner = *owner;
+}
+
