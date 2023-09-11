e520ddf149801b20b821d883ec1a82f142da5060
nono <nour98nour@gmail.com>
2023-09-11 16:28:27 -0700

    init 0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - define dog
+ *
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
