e0a86afc2611dd7c2df3338f20fc62abc56117d7
nono <nour98nour@gmail.com>
2023-09-11 18:13:03 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free char *
+ *
+ * @d: struct
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
