3e30e1fb813b85c039573fb609f43a4b49bf5f3d
nono <nour98nour@gmail.com>
2023-09-13 14:19:03 -0700

    1st task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
 * print_name_as_is - prints a name as is
 * @name: name of the person
 * @f: function pointer
-* 
+*
 * Return: Nothing.
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f (name);
+	f(name);
 }
