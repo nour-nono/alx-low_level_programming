dcd8151413a346d2a4ed5e681d4f3adc6909c3fa
nono <nour98nour@gmail.com>
2023-09-13 14:17:07 -0700

    init

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_name_as_is - prints a name as is
+* @name: name of the person
+* @f: function pointer
+* 
+* Return: Nothing.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f (name);
+}
