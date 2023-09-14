4f7f52b485ce3098fb8f26efa6f415addd108206
nono <nour98nour@gmail.com>
2023-09-13 18:34:55 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,20 +10,21 @@
 
 int main(int argc, char *argv[])
 {
-	int (*do_job)(int, int) = get_op_func(argv[2]);
-	int a = atoi(argv[1]), b = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		puts("Error");
 		exit(98);
 	}
+
+	int (*do_job)(int, int) = get_op_func(argv[2]);
+	int a = atoi(argv[1]), b = atoi(argv[3]);
+
 	if (do_job == NULL)
 	{
 		puts("Error");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		puts("Error");
 		exit(100);
