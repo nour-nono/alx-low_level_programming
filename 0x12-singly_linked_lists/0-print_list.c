012043cf32a57319ba3457fc578eeb1e7b76d1d6
nono <nour98nour@gmail.com>
2023-09-21 16:33:04 -0700

    Revert "-1 task"
    This reverts commit d9eb6f04359f4268b5424cdcd8c46553d75fadcf.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,5 @@
 #include "lists.h"
 /**
- *  _strlen - Entry point
- *
- * @s: is a pointer
- *
- * Description: this program prints "_putchar"
- *
- * Return: Always 0 (Success)
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		++i;
-	return (i);
-}
-/**
  * print_list - check the code
  * @h: struct
  * Return: Always int.
@@ -25,19 +8,15 @@ size_t print_list(const list_t *h)
 {
 	size_t cnt = 1;
 	const list_t *i = h;
-	int le;
 
 	if (!h)
 		return (0);
-	le = _strlen(i->str);
 	while (i->next != NULL)
 	{
-		le = _strlen(i->str);
-
-		(!i->str) ? printf("[0] (nil)\n") : printf("[%d] %s\n", le, i->str);
+		(!i->str) ? printf("[0] (nil)\n") : printf("[%d] %s\n", i->len, i->str);
 		i = i->next;
 		++cnt;
 	}
-	(!i->str) ? printf("[0] (nil)\n") : printf("[%d] %s\n", le, i->str);
+	(!i->str) ? printf("[0] (nil)\n") : printf("[%d] %s\n", i->len, i->str);
 	return (cnt);
 }
