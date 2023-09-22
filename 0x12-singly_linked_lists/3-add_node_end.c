6e2872d436925a71179df886fd83e6f2f015fa01
nono <nour98nour@gmail.com>
2023-09-21 18:33:27 -0700

    3 task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,6 +10,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *curr = malloc(sizeof(list_t));
 	list_t *ex = *head;
 
+	if (curr)
+		return (NULL);
 	while (ex && ex->next)
 		ex = ex->next;
 	curr->str = strdup(str);
