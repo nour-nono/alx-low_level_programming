d0e9e6e70c1b36588792cb9ce201e5810cf4c9cf
nono <nour98nour@gmail.com>
2023-09-25 12:54:55 -0700

    5-task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - check the code
+ * @head: pointer to pointer
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *x;
+
+	while (*head)
+	{
+		x = *head, *head = (*head)->next;
+		free(x);
+	}
+}
