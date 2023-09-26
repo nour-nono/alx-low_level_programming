72ba74a33d66afb2cffdec89a42efdb9bf258d7f
nono <nour98nour@gmail.com>
2023-09-26 09:33:26 -0700

    6 task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - check the code
+ * @head: pointer to pointer
+ * Return: Always 0.
+*/
+int pop_listint(listint_t **head)
+{
+	int y;
+	listint_t *x;
+
+	if (!head)
+		return (0);
+	x = *head;
+	*head=(*head)->next;
+	y = x->n;
+	free(x);
+	return (y);
+}
