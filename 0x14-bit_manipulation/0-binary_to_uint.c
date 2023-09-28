8c902c02f02ea58d3eebf14e25ccb6b666cb908a
nono <nour98nour@gmail.com>
2023-09-28 14:47:00 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
  * binary_to_uint - check the code
  * @b: pointer to pointer
  * Return: Always 0.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+	long int x = strlen(b), i = 0;
 	unsigned int ans = 0;
 
-	if (!b)
-		return (0);
-	for (; *b;)
+	--x;
+	for (; x >= 0; --x, ++i)
 	{
 		if (b[x] == '1')
-			ans = ans * 2 + 1;
+			ans += (1 << i);
 		else if (b[x] != '0')
 			return (0);
 	}
