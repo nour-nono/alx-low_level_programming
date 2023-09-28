6a571c0d0cd05c2c2c2556079c15fcf9b969510c
nono <nour98nour@gmail.com>
2023-09-28 14:23:54 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,21 @@
 #include "main.h"
+/**
+ *  _strlen - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		++i;
+	return (i);
+}
 
 /**
  * binary_to_uint - check the code
@@ -8,7 +25,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x, i = 0, ans = 0;
+	unsigned int i = 0, ans = 0;
+	long int x = 0;
 
 	if (!b)
 		return (0);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,23 +5,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-/**
- *  _strlen - Entry point
- *
- * @s: is a pointer
- *
- * Description: this program prints "_putchar"
- *
- * Return: Always 0 (Success)
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		++i;
-	return (i);
-}
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
