ac910ac38b0910d58d23cf2a6b9f0f16f7e54fd8
nono <nour98nour@gmail.com>
2023-09-28 16:06:41 -0700

    1 task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,21 @@
 */
 void print_binary(unsigned long int n)
 {
-    int size, i;
-    if (!n)
-        putchar('0');
-    else
-    {
-        size = 0, i = 0;
-	for (; i < 32; ++i)
-		if ((n >> i) & 1)
-			size = i;
-        for (; size >= 0; --size)
-        {
-            if ((n >> size) & 1)
-                putchar('1');
-            else
-                putchar('0');
-        }
-    }
+	int size, i;
+	if (!n)
+		putchar('0');
+	else
+	{
+		size = 0, i = 0;
+		for (; i < 32; ++i)
+			if ((n >> i) & 1)
+				size = i;
+		for (; size >= 0; --size)
+		{
+			if ((n >> size) & 1)
+				putchar('1');
+			else
+				putchar('0');
+		}
+	}
 }
