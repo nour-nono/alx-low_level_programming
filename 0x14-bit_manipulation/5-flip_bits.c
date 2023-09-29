daa36064e858702bf27acf13f1e44c2a7fe3fc83
nono <nour98nour@gmail.com>
2023-09-28 17:35:43 -0700

    5 task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * flip_bits - check the code
+ * @n: num
+ * @m: num
+ * Return: int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int ans1 = (n ^ m);
+	int ans2 = 0, i = 0;
+
+	for (; i < 63; i++)
+		if ((ans1 >> i) & 1)
+			++ans2;
+	return (ans2);
+}
