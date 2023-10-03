8d881fd1cd315397829c1128040dbf81d97cb4e0
nono <nour98nour@gmail.com>
2023-10-02 17:01:02 -0700

    0 task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * read_textfile - reading from another file
+ * @filename: file location and name
+ * @letters: bytes
+ * Return: Always 0.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t num = 0;
+	int fl;
+	char words[letters + 1];
+
+	if (!filename || !letters)
+		return (0);
+	fl = open(filename, O_RDONLY);
+	if (fl == -1)
+		return (0);
+	num = read(fl, words, letters);
+	num = write(STDOUT_FILENO, words, num);
+	close(fl);
+	return (num);
+}
