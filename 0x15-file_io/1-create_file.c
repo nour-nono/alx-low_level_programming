46f420b5a0662bc94c6be6ca509274a93c19aa68
nono <nour98nour@gmail.com>
2023-10-02 17:33:17 -0700

    1 task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ *  _strlen - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		++i;
+	}
+	return (i / sizeof(char));
+}
+/**
+ * create_file - Entry point
+ *
+ * @filename: is a pointer
+ *
+ * @text_content: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t length = _strlen(text_content), num;
+	int fl = 0;
+
+	if (!filename)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fl == -1)
+		return (-1);
+	if (length)
+	{
+		num = write(fl, text_content, length);
+	}
+	if (num == length)
+		return (1);
+	else
+		return (-1);
+}
