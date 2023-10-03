b0dea692453a791beddc20715f84d49c35623ab1
nono <nour98nour@gmail.com>
2023-10-02 18:44:41 -0700

    3 task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * @argc:num of aruments
+ *
+ * @argv:num of aruments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int from=0, to =0;
+	ssize_t num;
+	char cont[1024];
+
+	if (argc != 3 )
+		dprintf(STDERR_FILENO,"Usage: cp file_from file_to"), exit(97);
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE"), exit (98);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE"), exit (99);
+	while ((num = read(from, cont, 1024)) > 0)
+		if (write(to, cont, num) != num)
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE"), exit (99);
+	if (num = -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE"), exit (98);
+	close(from);
+	close(to);
+	if (from)
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE"), exit (100);
+	if (to)
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE"), exit (100);
+}
