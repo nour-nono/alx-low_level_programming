969a7223b13eac509de0c3bea765c639b074c7d1
nono <nour98nour@gmail.com>
2023-10-02 18:06:54 -0700

    1 task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ *  _strlen - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		++i;
+	return (i);
+}
+/**
+ * append_text_to_file - Entry point
+ *
+ * @filename: is a pointer
+ *
+ * @text_content: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t length = _strlen(text_content), num = 0;
+	int fl = 0;
+
+	if (!filename)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_APPEND);
+	if (fl == -1)
+		return (-1);
+	if (length)
+		num = write(fl, text_content, length);
+	close(fl);
+	if (num == length)
+		return (1);
+	else
+		return (-1);
+}
