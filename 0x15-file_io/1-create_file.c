37ac663de081dde69a7f19e519d4c6aab87786dd
nono <nour98nour@gmail.com>
2023-10-02 17:49:35 -0700

    1 task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | SIWUSR);
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fl == -1)
 		return (-1);
 	if (length)
