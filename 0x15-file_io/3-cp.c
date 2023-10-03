f0ac4009f6de1a7a5d7cc43bc4c3864d571d50d7
nono <nour98nour@gmail.com>
2023-10-02 19:11:35 -0700

    3 task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,28 +11,34 @@
  */
 int main(int argc, char *argv[])
 {
-	int from=0, to =0;
+	int from = 0, to = 0;
 	ssize_t num;
 	char cont[1024];
 
-	if (argc != 3 )
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n"), exit(97);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit (98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit (99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((num = read(from, cont, 1024)) > 0)
 		if (write(to, cont, num) != num)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit (99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (num == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit (98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	close(from);
 	close(to);
 	if (from)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit (100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 	if (to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit (100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 	return (EXIT_SUCCESS);
 }
