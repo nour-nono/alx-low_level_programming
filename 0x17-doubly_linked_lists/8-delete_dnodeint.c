48e01a6f36c97d53b5c17144a627e10b74c246c8
nono <nour98nour@gmail.com>
2023-11-09 13:26:02 -0800

    7 task

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - check the code
+ * @head: pointer to pointer
+ * @index: pointer to pointer
+ * Return: Always 0.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int cnt = 0;
+	dlistint_t *x, *y, *d;
+
+	if (!head || *head == NULL)
+		return (-1);
+	x = *head;
+	if (index == 0 && x->next != NULL)
+	{
+		if (x->next)
+			*head = x->next, (*head)->prev = NULL;
+		else
+			*head = NULL;
+		free(x);
+		return (1);
+	}
+	while (x->next && cnt != index)
+		x = x->next, ++cnt;
+	if (cnt != index)
+		return (-1);
+	x->prev->next = x->next;
+	if (x->next)
+		x->next->prev = x->prev;
+	free(x);
+	return (1);
+}
