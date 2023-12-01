6937b8ab2c093f7313e5ad36fcb606626671c3a9
nono <nour98nour@gmail.com>
2023-12-01 15:06:01 -0800

    3 task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL, *target = NULL;
+	hash_node_t *target = NULL;
 
 	if (!ht || !key)
 		return (0);
