6d17868bb281a9277f407d1042e317632bf9ff9d
nono <nour98nour@gmail.com>
2023-12-01 15:09:03 -0800

    3 task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
-	target = ht->array[key_index(key, ht->size)];
+	target = ht->array[key_index((const unsigned char *) key, ht->size)];
 	while (target && strcmp(target->key, key) != 0)
 		target = target->next;
 	target = malloc(sizeof(hash_node_t));
