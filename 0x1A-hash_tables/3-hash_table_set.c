1a194a0d5c477cd616fdc7dda64f16e59cfc840f
nono <nour98nour@gmail.com>
2023-12-01 17:09:57 -0800

    5 & 6 task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,6 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		target = target->next;
 	if (!target)
 	{
+		target = NULL;
 		target = malloc(sizeof(hash_node_t));
 		if (!target)
 			return (0);
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0, flag = 0;
 
 	putchar('{');
-	if (!ht)
+	if (!ht || !ht->array)
 	{
 		printf("}\n");
 		return;
