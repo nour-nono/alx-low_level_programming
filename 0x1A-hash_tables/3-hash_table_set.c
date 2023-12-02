8151e2d6b484d9edea910617db1d5af0bb152e65
nono <nour98nour@gmail.com>
2023-12-01 17:02:49 -0800

    5 & 6 task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,7 +38,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		free(target->value);
-		free(ht->array[key_index((const unsigned char *)key, ht->size)]->value);
 		target->value = strdup(value);
 	}
 	ht->array[key_index((const unsigned char *) key, ht->size)] = target;
