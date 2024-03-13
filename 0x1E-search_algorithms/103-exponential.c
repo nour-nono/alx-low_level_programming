1c0a14a2853de80b121f72afc37b88cee40f3202
nono <nour98nour@gmail.com>
2024-03-13 03:22:15 +0200

    added function binary search 2

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,39 @@
 #include "search_algos.h"
 
 /**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * @start: starting index
+ * @end: ending index
+ * Return: the first index where value is located, or -1 on failure
+ */
+int binary_search22(int *array, size_t size, int value, size_t start, size_t end)
+{
+	size_t i;
+	int mid;
+
+	if (array == NULL)
+		return (-1);
+	while (start <= end)
+	{
+		printf("Searching in array: ");
+		for (i = start; i < end; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		mid = (start + end) / 2;
+		if (array[mid] < value)
+			start = mid + 1;
+		else if (array[mid] > value)
+			end = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
+/**
  * exponential_search - searches for a value in a sorted array of integers usin
  * the Exponential search algorithm
  * @array: pointer to the first element of the array to search in
@@ -27,5 +60,5 @@ int exponential_search(int *array, size_t size, int value)
 	}
 	bound = i < size - 1 ? i : size - 1;
 	printf("Value found between indexes [%lu] and [%lu]\n", i / 2, bound);
-	return (binary_search(array, size, value, i / 2, bound));
+	return (binary_search2(array, size, value, i / 2, bound));
 }
