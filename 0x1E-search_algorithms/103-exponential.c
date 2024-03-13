acf7d4048ac42c4995cef68d6174a65e234162e9
nono <nour98nour@gmail.com>
2024-03-13 03:28:23 +0200

    fixed betty

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of integers using the
+ * binary_search2 - searches for a value in a sorted array of integers using the
  * Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @value: value to search for
