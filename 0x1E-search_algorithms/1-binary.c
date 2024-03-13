f6f0802f2ccf5f1d89c162ac9a76dee700ffbe3f
nono <nour98nour@gmail.com>
2024-03-13 02:37:27 +0200

    convert indentation to taps

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,26 +10,26 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i, left, right;
-    int mid;
+	size_t i, left, right;
+	int mid;
 
-    if (array == NULL)
-        return (-1);
-    left = 0;
-    right = size - 1;
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (i = left; i < right; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[i]);
-        mid = (left + right) / 2;
-        if (array[mid] < value)
-            left = mid + 1;
-        else if (array[mid] > value)
-            right = mid - 1;
-        else
-            return (mid);
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		mid = (left + right) / 2;
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
 }
