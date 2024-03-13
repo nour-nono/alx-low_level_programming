682e960620ca13690ccf4d6d612523c9dd94091b
nono <nour98nour@gmail.com>
2024-03-13 02:32:50 +0200

    added main files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -22,10 +24,12 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
 /*
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
 */
+
 listint_t *linear_skip(listint_t *list, int value);
 #endif
