524e8a16c3d2c07052ddb9a7a9d1db6da738a16e
nono <nour98nour@gmail.com>
2024-03-13 02:53:59 +0200

    fixed betty in header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -19,9 +19,9 @@ int advanced_binary(int *array, size_t size, int value);
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
